0fd0c489a5138a057930be845274e35841721178
Monir Hossen <monirhossen@pursuit.org>
2023-01-25 17:10:34 -0500

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  if (rolls.length === 0) {
+    return true
+  }
+  for (e of rolls) {
+
+  }  if (typeof e === "number") {
+    return true
+  }
+
+return false
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +22,14 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (e of rolls){
+    if (e === value){
+      return value 
+    }
+  }
+  return null
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +37,33 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  const arr = []
+  for (e of rolls){
+    if (e >= lowest)
+      arr.push(e)
+  }
+  return arr
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let result = {}
+
+  for (let i = 0; i < rolls.length; i++) {
+    if (result[rolls[i]]) {
+      result[rolls[i]] += 1
+    }else{
+      result[rolls[i]] = 1 
+    }
+    
+  }
+  return result
+}
 
 // Do not change the code below here.
 module.exports = {

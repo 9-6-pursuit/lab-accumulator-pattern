93f0b48187f39e748ecacccd185400cc4d330e3f
Jerry John <jerryjohn@Jerrys-MacBook-Air.local>
2023-02-11 23:23:35 -0500

    ROLLIN

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  for (const roll of rolls) {
+    if (typeof roll !== 'number') {
+      return false ;
+    }
+    
+  }
+  return true
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +19,18 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+
+  for (const roll of rolls) {
+    if (rolls === []) {
+    return 'null'
+  } else {
+   value
+  }
+ }
+return rolls
+}
+
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +38,35 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let lowRoll = []
+
+  for (const roll of rolls) {
+    if (roll >= lowest) {
+      lowRoll.push(roll)
+    }
+  }
+
+  return lowRoll
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+ let newObj = {};
+
+  for (const roll of rolls) {
+    if (newObj[roll]) {
+      newObj[roll] += 1
+    } else {
+      newObj[roll] = 1
+    }
+  }
+    return newObj
+}
 
 // Do not change the code below here.
 module.exports = {
